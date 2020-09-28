35e13ac295aa421bdaaf421651db6359a2fcba7c
Ismet Nasic <ismet_nasic@hotmail.com>
2020-09-28 05:47:43 +0200

    9

--- staribagrem/settings.py
+++ staribagrem/settings.py
@@ -16,6 +16,8 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -57,7 +59,8 @@ ROOT_URLCONF = 'staribagrem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        "DIRS": [os.path.join(os.path.dirname(__file__), 'templates')],
+        'DIRS': ['%s/templates/' % (PROJECT_DIR),
+                 '%s/dev_templates/' % (PROJECT_DIR),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
